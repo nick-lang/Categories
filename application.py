5abc3e377becb6ece66675ccc671ecad44af9efd
Nick Lang <saltblade@gmail.com>
2017-10-04 17:18:04 -0600

    added route placeholders and templates directory

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Categories, Books
+from catalogdb import get_books, add_book
 
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.create_all(engine)
@@ -12,17 +13,43 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home/')
 def Home():
     return "Home"
 
-@app.route('/categories')
+@app.route('/categories/')
 def ListCategories():
     categories = session.query(Categories).all()
     output = ''
     for i in categories:
         output += i.name
         output += '</br>'
+    return output
+
+@app.route('/categories/<int:category_id>/')
+def ListBooks(category_id):
+    books = get_books()
+    output = ''
+    for i in books:
+        output += i.name
+        output += '</br>'
+        output += i.description
+        output += '</br>'
+    return output
+
+@app.route('/categories/<int:category_id>/new/')
+def newBook(category_id):
+    output = 'new book'
+    return output
+
+@app.route('/categories/<int:category_id>/<int:book_id>/edit/')
+def editBook(category_id, book_id):
+    output = 'edit'
+    return output
+
+@app.route('/categories/<int:category_id>/<int:book_id>/delete/')
+def deleteBook(category_id, book_id):
+    output = 'delete'
     return output
 
 if __name__ == '__main__':
