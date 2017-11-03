568587acd82ad9d14b42fa916d5752fe3f3d1bb2
Nick Lang <saltblade@gmail.com>
2017-11-03 13:01:23 -0600

    In process of adding login functionality

--- application.py
+++ application.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, request, url_for, jsonify
 app = Flask(__name__)
 
-from flask import render_template
+from flask import session as login_session
+import random, string
+
+from oath2client.client import flow_from_clientsecrets, FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Categories, Books
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
 
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.create_all(engine)
@@ -79,6 +89,14 @@ def listBooksJSON(category_id):
     books = session.query(Books).filter_by(category_id = category_id).all()
     return jsonify(Books=[i.serialize for i in books])
 
+@app.route('/login/')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase
+                    + string.digits) for x in xrange(32))
+    login_session['state'] = state
+    return render_template('login.html')
+
 if __name__ == '__main__':
+    app.secret_key = 'Super secret key'
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
