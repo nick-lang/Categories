e1a168b2d6395fd84ce3bce3d215d9bf2caef4ab
Nick Lang <saltblade@gmail.com>
2017-11-28 09:57:58 -0700

    Added comments to login. Added ability to disconnect from Google login.

--- application.py
+++ application.py
@@ -89,6 +89,8 @@ def listBooksJSON(category_id):
     books = session.query(Books).filter_by(category_id = category_id).all()
     return jsonify(Books=[i.serialize for i in books])
 
+###Google login/logout logic taken from "Creating Google Sign in"
+###portion of Udacity course
 @app.route('/login/')
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase
@@ -179,6 +181,38 @@ def gconnect():
     output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
     flash("you are now logged in as %s" % login_session['username'])
     return output
+
+# DISCONNECT - Revoke a current user's token and reset their login_session
+@app.route('/gdisconnect')
+def gdisconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print 'Access Token is None'
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    print 'In gdisconnect access token is %s', access_token
+    print 'User name is: '
+    print login_session['username']
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print 'result is '
+    print result
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
 
 if __name__ == '__main__':
     app.secret_key = 'Super secret key'
